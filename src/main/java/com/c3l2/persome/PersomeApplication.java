a2ff2dcfe7e296671f22b25b756266b3925b36d5
dlthdbs03 <dlsth13@gmail.com>
2025-09-12 10:37:17 +0900

    Initial commit

---
+++ src/main/java/com/c3l2/persome/PersomeApplication.java
@@ -0,0 +1,13 @@
+package com.c3l2.persome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c3l2/persome/PersomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c3l2.persome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
