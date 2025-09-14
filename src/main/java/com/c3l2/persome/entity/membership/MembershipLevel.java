39dc56d944defad2fcc842f103e728b4cb5c8e55
dlthdbs03 <dlsth13@gmail.com>
2025-09-15 00:24:03 +0900

    [feat] Membership 관련 entity 구현

---
+++ src/main/java/com/c3l2/persome/entity/membership/MembershipLevel.java
@@ -0,0 +1,29 @@
+package com.c3l2.persome.entity.membership;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "membership_level")
+@Entity
+public class MembershipLevel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name",  nullable = false)
+    private Name name;
+
+    @Column(name = "min_amount",  nullable = false)
+    private Integer minAmount;
+
+    @Column(name = "max_amount")
+    private Integer maxAmount;
+}
---
+++ src/main/java/com/c3l2/persome/entity/membership/Name.java
@@ -0,0 +1,9 @@
+package com.c3l2.persome.entity.membership;
+
+public enum Name {
+    BABY,
+    PINK,
+    GREEN,
+    BLACK,
+    GOLD
+}
