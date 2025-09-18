630fc5432787ec27f39c1d33bd18d08e0ee12dfe
hyeeuncho <heeuncho@naver.com>
2025-09-18 10:39:37 +0900

    [feat] 주문 1시간 뒤 결제완료 로직 구현

--- src/main/java/com/c3l2/persome/PersomeApplication.java
+++ src/main/java/com/c3l2/persome/PersomeApplication.java
@@ -2,8 +2,10 @@ package com.c3l2.persome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PersomeApplication {
 
 	public static void main(String[] args) {
