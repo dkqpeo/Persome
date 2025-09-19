8a03638c22383393ddeaad27332cee8b9a1159ca
dlthdbs03 <dlsth13@gmail.com>
2025-09-19 18:56:24 +0900

    [feat] 홈, 로그인, 회원가입 뷰 구현

---
+++ src/main/java/com/c3l2/persome/home/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.c3l2.persome.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home"; // templates/home.html
+    }
+}
