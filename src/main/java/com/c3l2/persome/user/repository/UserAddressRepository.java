c3874c8c63a98d7b356d6ac92a5faee9578aeac5
seung_wook <wook0984@gmail.com>
2025-09-19 11:41:48 +0900

    [feat] 배송지 수정/삭제 API 구현3

--- src/main/java/com/c3l2/persome/user/repository/UserAddressRepository.java
+++ src/main/java/com/c3l2/persome/user/repository/UserAddressRepository.java
@@ -2,9 +2,11 @@ package com.c3l2.persome.user.repository;
 
 import com.c3l2.persome.user.entity.UserAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserAddressRepository extends JpaRepository<UserAddress, Long> {
 
     Optional<UserAddress> findByIdAndUserId(Long id, Long userId);
