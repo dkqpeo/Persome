aa2883c73be50e043353802005d077fd12fa6909
dlthdbs03 <dlsth13@gmail.com>
2025-09-25 10:06:12 +0900

    [refact] 표인트/위시리스트 뷰컨트롤러 마이페이지컨트롤러로 이동

--- src/main/java/com/c3l2/persome/user/controller/MyPageController.java
+++ src/main/java/com/c3l2/persome/user/controller/MyPageController.java
@@ -2,8 +2,10 @@ package com.c3l2.persome.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/mypage")
 public class MyPageController {
 
     @GetMapping({"/mypage", "/mypage/"})
@@ -25,4 +27,10 @@ public class MyPageController {
     public String myCouponsPage() {
         return "/mypage/coupons";
     }
+
+    @GetMapping({"/points", "/points/"})
+    public String myPointsPage() { return "mypage/points"; }
+
+    @GetMapping({"/wishlist", "/wishlist/"})
+    public String wishlistPage() { return "mypage/wishlist"; }
 }
