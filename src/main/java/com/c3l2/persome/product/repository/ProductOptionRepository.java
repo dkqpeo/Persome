0e2cb8803403661f0f7649c697aa8f3a72f31a02
dkqpeo <lan0184@naver.com>
2025-09-26 14:28:01 +0900

    [test] 리뷰 테스트 작성

--- src/main/java/com/c3l2/persome/product/repository/ProductOptionRepository.java
+++ src/main/java/com/c3l2/persome/product/repository/ProductOptionRepository.java
@@ -2,8 +2,15 @@ package com.c3l2.persome.product.repository;
 
 import com.c3l2.persome.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+
+    @Override
+    @Query("SELECT p FROM ProductOption p  LEFT JOIN FETCH p.product WHERE p.id = :id")
+    Optional<ProductOption> findById(Long id);
 }
