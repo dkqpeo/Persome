397bbb651ffdf9db714de1713d46a28f283ba04a
dlthdbs03 <dlsth13@gmail.com>
2025-09-29 12:24:26 +0900

    [feat] Event 관련 SQL

---
+++ src/main/resources/sql/5-1.event_img.sql
@@ -0,0 +1,5 @@
+insert into event_img (img_order, event_id, id, img_url) values (1, 1, 1, '/images/event/chuseok_banner1.jpg');
+insert into event_img (img_order, event_id, id, img_url) values (1, 2, 2, '/images/event/fall_school_banner1.jpg');
+insert into event_img (img_order, event_id, id, img_url) values (1, 3, 3, '/images/event/xmas_banner1.jpg');
+insert into event_img (img_order, event_id, id, img_url) values (1, 4, 4, '/images/event/newyear_banner1.jpg');
+insert into event_img (img_order, event_id, id, img_url) values (1, 5, 5, '/images/event/spring_banner1.jpg');
